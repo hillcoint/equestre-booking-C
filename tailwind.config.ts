319cc0732186c4226ca990b25f09a67339547116
hillcoint <78450516+hillcoint@users.noreply.github.com>
2026-09-19 09:22:53 +0200

    Configure Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  theme: {
+    container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
